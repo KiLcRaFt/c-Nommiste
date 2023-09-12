c49c5b6908199283c73c57094dcf48f397625348
opilane <opilane@HP-CZC2349HTZ>
2023-09-12 16:32:47 +0300

    gg

---
+++ EsimeneProjekt/Group.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulesane_2_Nommiste
+{
+    public class Group
+    {
+        public List<string> Members { get; } = new List<string>();
+        public List<string> Age { get; } = new List<string>();
+        private readonly int _maxAmount;
+
+        public Group(int maxAmount)
+        {
+            _maxAmount = maxAmount;
+        }
+
+        public bool AddMember(string member)
+        {
+            if (Members.Contains(member)) return false;
+            if (Members.Count >= _maxAmount) return false;
+            Members.Add(member);
+            AddAge();
+            return true;
+        }
+
+        public bool AddAge()
+        {
+            Console.WriteLine("Sisseta inimese vana: ");
+            string age = Console.ReadLine();
+            if (Age.Count >= _maxAmount) return false;
+            Age.Add(age);
+            return true;
+        }
+
+        public int GetMembersCount()
+        {
+            return Members.Count;
+        }
+
+        public bool HasMember(string member)
+        {
+            return Members.Contains(member);
+        }
+
+    }
+}
--- EsimeneProjekt/Program.cs
+++ EsimeneProjekt/Program.cs
@@ -1,55 +1,42 @@
-﻿using static Ulesane_2_Nommiste.Programmbaseee;
-
-namespace Ulesane_2_Nommiste
+﻿namespace Ulesane_2_Nommiste
 {
-    internal class Program
+   public class Program
     {
-        public static void Main(string[] args)
-        {
-            //Console.WriteLine("1. Add group member \n2. Check group member \n3. Add member in BookClub \n4. Check member in BookClub");
-            //string ans = Console.ReadLine();
-
-            Random rnd = new Random();
-            Group group = new Group(3);
-
+        public static void Main()
+        {   
             Console.WriteLine("Kui palju inimest: ");
-            string groupmembers = Console.ReadLine();  
+            string groupmembers = Console.ReadLine();
             int grupmem = Convert.ToInt32(groupmembers);
 
-            for (int n = 0; n < grupmem; n++)
+            Group group = new Group(grupmem);
+            int n = 0;
+            while(n < grupmem)
             {
                 Console.WriteLine("Sisseta inimese nimi: ");
                 string nimi = Console.ReadLine();
-                Console.WriteLine(group.AddMember(nimi));
-                //Memember m1 = new Memember(nimi, DateTime.Now);
-            };
+                if (group.AddMember(nimi))
+                {
+                    Console.WriteLine(group.AddMember(nimi));
+                    n++;
+                }
+                else {
+                    Console.WriteLine("Error");
+                }
 
+            }
+            //for (int n = 0; n < grupmem )
+            //{
+            //    Console.WriteLine("Sisseta inimese nimi: ");
+            //    string nimi = Console.ReadLine();
+            //    Console.WriteLine(group.AddMember(nimi));
+            //};
             Console.WriteLine();
-            Console.WriteLine(string.Join(", ", group.Members));  // John, Mary, Jane
-            Console.WriteLine("Grupisse on {0} inimest.", group.GetMembersCount());  // 3
-            Console.WriteLine();
-
-            Group bookClub = new Group(4);
-            
-            Console.WriteLine("Kui palju inimest: ");
-            string clubmembers = Console.ReadLine();
-            int clubmem = Convert.ToInt32(clubmembers);
-            
-
-            for (int n = 0; n < clubmem; n++)
+            for (int y = 0; y < group.GetMembersCount(); y++)
             {
-                Console.WriteLine("Sisseta inimese nimi: ");
-                string nimi = Console.ReadLine();
-                Console.WriteLine(bookClub.AddMember(nimi));
-                //Memember m1 = new Memember(nimi, DateTime.Now);
+                Console.WriteLine(string.Join("Name: ", group.Members[n]) + " " + string.Join("Type: ", group.Age[n]));
             };
-
             Console.WriteLine();
-            Console.WriteLine(string.Join(", ", bookClub.Members));  // Albert, Samantha, Robert, Roberta
-            Console.WriteLine("Raamatu kluubis on {0} inimest.", bookClub.GetMembersCount());  // 4
-            Console.WriteLine();
-            Console.WriteLine(bookClub.HasMember("Albert"));  // True
-            Console.WriteLine(bookClub.HasMember("John"));  // False
+            Console.WriteLine("Grupisse on " + group.GetMembersCount() + " inimest\n");
         }
     }
 }
