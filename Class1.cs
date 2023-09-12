b389b6109ead7e80357f04e3c7159377ba029e73
M0RGAN <M0RGAN@DESKTOP-P09LV9A>
2023-09-12 16:17:39 +0300

    gg

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+    public class Person
+    {
+        public string name { get; }
+        public int age;
+
+        public person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public int BDate()
+        {
+            int bdate = DateTime.Now.Year;
+            int Result = bdate - age;
+            return Result;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(BDate());
+
+        }
+    }
