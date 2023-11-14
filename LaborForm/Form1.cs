59a597b2a9117ba53ff74881eb1ae06731bcd78f
opilane <opilane@HP-CZC2349HT2>
2023-11-14 11:38:36 +0200

    done

--- LaborForm/Form1.cs
+++ LaborForm/Form1.cs
@@ -1,42 +1,117 @@
+using System.Web;
+
 namespace LaborForm
 {
     public partial class Form1 : Form
     {
         Label lbl1, lbl2, lbl3, lbl4, lbl5, lbl6;
         PictureBox pb;
-        //Point point;
-        //bool BtnClicked = false;
-        //string lbl = "lbl";
-        //int posX, posY;
+        Button checkBtn;
+        int LastClicked;
+        bool ans1, ans2, ans3;
         public Form1()
         {
             this.Height = 450;
             this.Width = 400;
-            this.Text = "Labor";
-            
-            lbl1= new Label();
+            this.Text = "Matemaatika test";
+            this.StartPosition= FormStartPosition.CenterScreen;
+
+            Random rnd = new Random();
+
+            List<string> primers = new List<string>();
+            primers.Add("2+2=");
+            primers.Add("6-1=");
+            primers.Add("2*3=");
+            primers.Add("14/2=");
+            primers.Add("4+2*2=");
+            primers.Add("3+3+3=");
+            primers.Add("100-9*10=");
+            primers.Add("11*1=");
+            primers.Add("12/1=");
+
+            List<string> answers = new List<string>();
+            answers.Add("4");
+            answers.Add("5");
+            answers.Add("6");
+            answers.Add("7");
+            answers.Add("8");
+            answers.Add("9");
+            answers.Add("10");
+            answers.Add("11");
+            answers.Add("12");
+
+            int ind1 = rnd.Next(0, answers.Count);
+            int ind2 = rnd.Next(0, answers.Count);
+            int ind3 = rnd.Next(0, answers.Count);
+            while (true)
+            {
+                if (ind2 == ind1 || ind2 == ind3)
+                {
+                    ind2 = rnd.Next(0, answers.Count);
+                }
+                if (ind3 == ind1 || ind3 == ind2)
+                {
+                    ind3 = rnd.Next(0, answers.Count);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            lbl1 = new Label();
             LabelChoise labl = new LabelChoise();
-            labl.Labl(lbl1, "4", 150, 60);
+            lbl1.AutoSize = true;
+            lbl1.Font = new Font("Tahoma", 36);
+            labl.Labl(lbl1, answers[ind1], 250, 260);
             labl.DragAnDrop();
+            lbl1.MouseUp += Lbl1_MouseUp;
             this.Controls.Add(lbl1);
 
             lbl2 = new Label();
             LabelChoise labl2 = new LabelChoise();
-            labl2.Labl(lbl2, "5", 150, 80);
+            lbl2.AutoSize = true;
+            lbl2.Font = new Font("Tahoma", 36);
+            labl2.Labl(lbl2, answers[ind2], 250, 60);
             labl2.DragAnDrop();
+            lbl2.MouseUp += Lbl2_MouseUp;
             this.Controls.Add(lbl2);
 
             lbl3 = new Label();
             LabelChoise labl3 = new LabelChoise();
-            labl3.Labl(lbl3, "6", 150, 100);
+            lbl3.AutoSize = true;
+            lbl3.Font = new Font("Tahoma", 36);
+            labl3.Labl(lbl3, answers[ind3], 250, 160);
             labl3.DragAnDrop();
+            lbl3.MouseUp += Lbl3_MouseUp;
             this.Controls.Add(lbl3);
 
             lbl4 = new Label();
             LabelChoise labl4 = new LabelChoise();
-            labl4.Labl(lbl4, "2+2=", 60, 60);
+            lbl4.AutoSize = true;
+            lbl4.Font = new Font("Tahoma", 36);
+            labl4.Labl(lbl4, primers[ind1], 60, 60);
             labl4.DragAnDrop();
+            lbl4.MouseUp += Lbl1_MouseUp;
             this.Controls.Add(lbl4);
+
+            lbl5 = new Label();
+            LabelChoise labl5 = new LabelChoise();
+            lbl5.AutoSize = true;
+            lbl5.Font = new Font("Tahoma", 36);
+            labl5.Labl(lbl5, primers[ind2], 60, 160);
+            labl5.DragAnDrop();
+            lbl5.MouseUp += Lbl2_MouseUp;
+            this.Controls.Add(lbl5);
+
+            lbl6 = new Label();
+            LabelChoise labl6 = new LabelChoise();
+            lbl6.AutoSize = true;
+            lbl6.Font = new Font("Tahoma", 36);
+            labl6.Labl(lbl6, primers[ind3], 60, 260);
+            labl6.DragAnDrop();
+            lbl6.MouseUp += Lbl3_MouseUp;
+            this.Controls.Add(lbl6);
 
 
             pb = new PictureBox();
@@ -45,71 +120,130 @@ namespace LaborForm
             pb.BorderStyle = BorderStyle.Fixed3D;
             this.Controls.Add(pb);
 
+            checkBtn = new Button();
+            checkBtn.Location = new Point(275, 360);
+            checkBtn.Text = "Kontroll";
+            checkBtn.Click += CheckBtn_Click;
 
-            //List<string> answernames = new List<string>();
-            //answernames.Add("4");
-            //answernames.Add("5");
-            //answernames.Add("6");
-
-            //List<string> names = new List<string>();
-            //answernames.Add("2+2=");
-            //answernames.Add("10-5=");
-            //answernames.Add("3*2=");
+            this.Controls.Add(checkBtn);
 
 
-            //for (int i = 0; i <= 5; i++)
-            //{
-
-            //    Label name  = lbl + Convert.ToString(i);
-            //    lbl1 = new Label();
-            //    lbl1.Text = "2+2=";
-            //    lbl1.Location = new Point(posX, posY);
-            //    lbl1.Size = new Size(180, 160);
-            //    lbl1.AutoSize = true;
-            //    lbl1.MouseDown += Lbl_MouseDown;
-            //    lbl1.MouseUp += Lbl_MouseUp;
-            //    lbl1.MouseMove += Lbl_MouseMove;
-            //    this.Controls.Add(lbl1);
-
-            //    posX += 10;
-            //    posY += 10;
-            //}
+            startmenu();
 
         }
 
+        private void CheckBtn_Click(object? sender, EventArgs e)
+        {
+            if (ans1 && ans2 && ans3)
+            {
 
+                lbl1.BackColor = Color.LightGreen;
+                lbl2.BackColor = Color.LightGreen;
+                lbl3.BackColor = Color.LightGreen;
+                lbl4.BackColor = Color.LightGreen;
+                lbl5.BackColor = Color.LightGreen;
+                lbl6.BackColor = Color.LightGreen;
 
-        //private void Lbl_MouseMove(object? sender, MouseEventArgs e)
-        //{
-        //    if (BtnClicked)
-        //    {
-        //        Point newPoint = lbl1.PointToScreen(new Point(e.X, e.Y));
-        //        newPoint.Offset(point);
-        //        lbl1.Location = newPoint;
-        //    }
-        //}
+                string message = "Kõik on täesti õige!";
+                string caption = "Matemaatika test";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
 
-        //private void Lbl_MouseUp(object? sender, MouseEventArgs e)
-        //{
-        //    BtnClicked = false;
-        //}
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                string message = "Midagi on valesti.";
+                string caption = "Matemaatika test";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
 
-        //private void Lbl_MouseDown(object? sender, MouseEventArgs e)
-        //{
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    ans1= false;
+                    ans2= false;
+                    ans3= false;
+                    lbl1.Location = new Point(250, 260);
+                    lbl2.Location = new Point(250, 60);
+                    lbl3.Location = new Point(250, 160);
+                    lbl4.Location = new Point(60, 60);
+                    lbl5.Location = new Point(60, 160);
+                    lbl6.Location = new Point(60, 260);
+                }
+            }
 
-        //    if (e.Button == MouseButtons.Left)
-        //    {
-        //        BtnClicked = true;
-        //        Point ptStartPosition = lbl1.PointToScreen(new Point(e.X, e.Y));
+        }
 
-        //        point = new Point();
-        //        point.X = lbl1.Location.X - ptStartPosition.X;
-        //        point.Y = lbl1.Location.Y - ptStartPosition.Y;
-        //    }
-        //    else
-        //    {
-        //        BtnClicked = false;
-        //    }
-        //}
+        private void startmenu()
+        {
+            string message = "Selle testi lahendamiseks peate soovitud vastuse näite juurde lohistama.";
+            string caption = "Matemaatika test";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void LastClicked_func()
+        {
+            if (LastClicked == 1)
+            {
+                if ((lbl1.Location.X < lbl4.Location.X + lbl4.Width) && (lbl1.Location.X > lbl4.Location.X))
+                {
+                    if ((lbl1.Location.Y > lbl4.Location.Y) && (lbl1.Location.Y < lbl4.Location.Y + lbl4.Height))
+                    {
+                        ans1 = true;
+                    }
+                }
+            }
+            if (LastClicked== 2)
+            {
+                if ((lbl2.Location.X < lbl5.Location.X + lbl5.Width) && (lbl2.Location.X > lbl5.Location.X))
+                {
+                    if ((lbl2.Location.Y > lbl5.Location.Y) && (lbl2.Location.Y < lbl5.Location.Y + lbl5.Height))
+                    {
+                        ans2 = true;
+                    }
+                }
+            }
+            if (LastClicked== 3)
+            {
+                if ((lbl3.Location.X < lbl6.Location.X + lbl6.Width) && (lbl3.Location.X > lbl6.Location.X))
+                {
+                    if ((lbl3.Location.Y > lbl6.Location.Y) && (lbl3.Location.Y < lbl6.Location.Y + lbl6.Height))
+                    {
+                        ans3 = true;
+                    }
+                }
+            }
+        }
+
+        private void Lbl3_MouseUp(object? sender, MouseEventArgs e)
+        {
+            LastClicked = 3;
+            LastClicked_func();
+        }
+
+        private void Lbl2_MouseUp(object? sender, MouseEventArgs e)
+        {
+            LastClicked = 2;
+            LastClicked_func();
+        }
+
+        private void Lbl1_MouseUp(object? sender, MouseEventArgs e)
+        {
+            LastClicked = 1;
+            LastClicked_func();
+
+        }
     }
 }
