9427347c44ac06411bf746769d192e98eb5eca9e
opilane <opilane@HP-CZC2349HTM>
2024-02-22 12:45:14 +0200

    vmc_3-0

---
+++ VMC3_0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VMC3_0.Startup))]
+namespace VMC3_0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
