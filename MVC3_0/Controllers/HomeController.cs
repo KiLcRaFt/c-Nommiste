fe7a62f0019e761765506a7f4438b20925c5d87d
M0RGAN <M0RGAN@DESKTOP-P09LV9A>
2024-02-27 18:47:26 +0300

    Первая часть готова (без подключения бд)

---
+++ MVC3_0/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using MVC3_0.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MVC3_0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootaan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Ankeet(Guest guest)
+        {
+            E_mail(guest);
+            if (ModelState.IsValid) { return View("Thanks", guest); }
+            else { return View(); }
+        }
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "goslingrayan925@gmail.com";
+                WebMail.Password = "wezm cjak kfkm hvcp ";
+                WebMail.From = "goslingrayan925@gmail.com";
+                WebMail.Send("martinnm84@gmail.com", "Vastus kutsele", guest.Name + " vastus " + ((guest.WillAttend ?? false) ? "tuleb peole " : "ei tule peole"));
+                ViewBag.Message = "Kiri on saatnud!";
+
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Mul on kahju! Ei saa kirja saada!!!";
+            }
+        }
+    }
+}
